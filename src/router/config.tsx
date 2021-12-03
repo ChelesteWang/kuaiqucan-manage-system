3771d03be9220195491cc8b2584a1b1445325930
Xinyuan Wang <40495740+ChelesteWang@users.noreply.github.com>
2021-12-03 08:45:31 +0800

    Initial commit

---
+++ src/router/config.tsx
@@ -0,0 +1,30 @@
+import React, { FC, Suspense } from 'react'
+import { Route } from 'react-router-dom'
+import { RouteProps } from 'react-router'
+import PrivateRoute from './pravateRoute'
+import SuspendFallbackLoading from '@src/components/fallback-loading'
+
+export interface WrapperRouteProps extends RouteProps {
+	/** document title id */
+	titleId: string
+	/** authorization？ */
+	auth?: boolean
+}
+
+const WrapperRouteComponent: FC<WrapperRouteProps> = ({ titleId, auth, ...props }) => {
+	const WitchRoute = auth ? PrivateRoute : Route
+	if (titleId) {
+		document.title = titleId
+	}
+	return <WitchRoute {...props} />
+}
+
+const WrapperRouteWithOutLayoutComponent: FC<WrapperRouteProps> = ({ titleId, auth, ...props }) => {
+	if (titleId) {
+		document.title = titleId
+	}
+
+	return <Suspense fallback={<SuspendFallbackLoading message="正在加载中" />}>{props.element}</Suspense>
+}
+
+export { WrapperRouteComponent, WrapperRouteWithOutLayoutComponent }
